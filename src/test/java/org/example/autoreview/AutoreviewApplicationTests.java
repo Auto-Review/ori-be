d8323effd9eac732986ccb3943303bcd1deb90af
신종신 <jongsin16@naver.com>
2024-09-30 13:20:31 +0900

    first commit

---
+++ src/test/java/org/example/autoreview/AutoreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.autoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
