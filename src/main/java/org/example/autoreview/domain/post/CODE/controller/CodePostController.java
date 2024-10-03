903ef2e42153c9b4f8bd5fbe253b3d8ad6280075
ehgur <ehgur062300@gmail.com>
2024-10-03 16:46:51 +0900

    refactor: swagger 옵션 추가

--- src/main/java/org/example/autoreview/domain/post/CODE/controller/CodePostController.java
+++ src/main/java/org/example/autoreview/domain/post/CODE/controller/CodePostController.java
@@ -1,5 +1,6 @@
 package org.example.autoreview.domain.post.CODE.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.autoreview.domain.post.CODE.dto.request.CodePostSaveRequestDto;
@@ -20,26 +21,31 @@ public class CodePostController {
 
     private final CodePostService codePostService;
 
+    @Operation(summary = "코드 포스트 생성", description = "코드 포스트 생성")
     @PostMapping("/api/v1/post/save")
     public ApiResponse<Long> save(@RequestBody CodePostSaveRequestDto requestDto){
         return ApiResponse.success(HttpStatus.OK, codePostService.save(requestDto));
     }
 
+    @Operation(summary = "코드 포스트 단일 조회", description = "코드 포스트 단일 조회")
     @GetMapping("/api/v1/post/code/view")
     public ApiResponse<CodePostResponseDto> view(@RequestParam Long codePostId){
         return ApiResponse.success(HttpStatus.OK, codePostService.findById(codePostId));
     }
 
+    @Operation(summary = "코드 포스트 전체 조회", description = "코드 포스트 전체 조회")
     @GetMapping("/api/v1/post/code/view-all")
     public ApiResponse<List<CodePostResponseDto>> viewAll(){
         return ApiResponse.success(HttpStatus.OK, codePostService.findAll());
     }
 
+    @Operation(summary = "코드 포스트 수정", description = "코드 포스트 수정")
     @PutMapping("/api/v1/post/code/update")
     public ApiResponse<Long> update(@RequestBody CodePostUpdateRequestDto requestDto) {
         return ApiResponse.success(HttpStatus.OK, codePostService.update(requestDto));
     }
 
+    @Operation(summary = "코드 포스트 삭제", description = "코드 포스트 삭제")
     @DeleteMapping("/api/v1/post/code/delete")
     public ApiResponse<Long> delete(@RequestParam Long codePostId){
         return ApiResponse.success(HttpStatus.OK, codePostService.delete(codePostId));
