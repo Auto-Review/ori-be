eeec81f348fa29a9a5f2eda72e251e0fb99e5791
ehgur <ehgur062300@gmail.com>
2024-10-21 00:25:07 +0900

    feat: 알림 저장 dto 생성

--- src/main/java/org/example/autoreview/domain/notification/dto/request/NotificationSaveRequestDto.java
+++ src/main/java/org/example/autoreview/domain/notification/dto/request/NotificationSaveRequestDto.java
@@ -3,16 +3,21 @@ package org.example.autoreview.domain.notification.dto.request;
 import org.example.autoreview.domain.notification.domain.Notification;
 import org.example.autoreview.domain.notification.enums.NotificationStatus;
 
+import java.time.LocalDateTime;
+
 public class NotificationSaveRequestDto {
 
     private String content;
 
     private NotificationStatus status;
 
+    private LocalDateTime executeTime;
+
     public Notification toEntity(){
         return Notification.builder()
                 .content(content)
                 .status(status)
+                .executeTime(executeTime)
                 .build();
     }
 }
