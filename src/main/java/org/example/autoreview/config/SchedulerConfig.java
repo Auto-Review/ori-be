86f770338aff5579001d8834207ba8ef094df7ec
ehgur <ehgur062300@gmail.com>
2024-10-23 02:29:58 +0900

    feat: 스케줄러 세팅 설정

--- src/main/java/org/example/autoreview/config/SchedulerConfig.java
+++ src/main/java/org/example/autoreview/config/SchedulerConfig.java
@@ -1,22 +1,53 @@
 package org.example.autoreview.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/**
+ * packageName    : org.example.autoreview.config.scheduler
+ * fileName       : ScheduleConfig
+ * author         : ehgur
+ * date           : 24. 10. 23
+ * description    : 스케줄러 설정 세팅
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 23.        ehgur            최초 생성
+ */
 
 @Configuration
-public class SchedulerConfig {
-
-    private static final int POOL_SIZE = 3;
-
+@EnableAsync
+@EnableScheduling
+@Slf4j
+public class SchedulerConfig implements SchedulingConfigurer {
     @Bean
-    public ThreadPoolTaskScheduler taskScheduler(){
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+    public ThreadPoolTaskScheduler configureTasks() {
+        log.info("configureTasks start");
 
-        taskScheduler.setPoolSize(POOL_SIZE);
-        taskScheduler.setThreadNamePrefix("My Scheduler-");
-        taskScheduler.initialize();
+        ThreadPoolTaskScheduler taskExecutor = new ThreadPoolTaskScheduler();
 
-        return taskScheduler;
+        taskExecutor.setPoolSize(3);
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setAwaitTerminationSeconds(20);
+        taskExecutor.setThreadNamePrefix("scheduled-task-");
+
+        log.info("configureTasks end");
+
+        return taskExecutor;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        log.info("configureTasks start taskRegistrar : {}", taskRegistrar);
+
+        taskRegistrar.setTaskScheduler(configureTasks());
+
+        log.info("configureTasks end taskRegistrar : {}", taskRegistrar);
     }
 }
