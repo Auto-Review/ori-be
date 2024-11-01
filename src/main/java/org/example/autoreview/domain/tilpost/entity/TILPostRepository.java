5f51f764aafe8d45b62302266a56a824895fe299
신종신 <jongsin16@naver.com>
2024-11-01 09:38:46 +0900

    feat: cursor paging 초기 구현 완료

--- src/main/java/org/example/autoreview/domain/tilpost/entity/TILPostRepository.java
+++ src/main/java/org/example/autoreview/domain/tilpost/entity/TILPostRepository.java
@@ -2,8 +2,17 @@ package org.example.autoreview.domain.tilpost.entity;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TILPostRepository extends JpaRepository<TILPost,Long> {
     Page<TILPost> findAll(Pageable pageable);
+
+    List<TILPost> findAllByOrderByIdDesc(Pageable pageable);
+    List<TILPost> findByIdLessThanOrderByIdDesc(@Param("id") long id, Pageable pageable);
+
 }
