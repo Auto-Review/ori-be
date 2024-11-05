74a5d95d044c3b0dd2e9214b01910632c86ca00c
ehgur062300 <ehgur062300@gmail.com>
2024-11-05 14:37:44 +0900

    fix: record 클래스는 @Getter가 필요없기에 제거

--- src/main/java/org/example/autoreview/domain/review/dto/request/ReviewSaveRequestDto.java
+++ src/main/java/org/example/autoreview/domain/review/dto/request/ReviewSaveRequestDto.java
@@ -1,9 +1,7 @@
 package org.example.autoreview.domain.review.dto.request;
 
-import lombok.Getter;
 import org.example.autoreview.domain.review.entity.Review;
 
-@Getter
 public record ReviewSaveRequestDto(
         Long codePostId,
         String code,
