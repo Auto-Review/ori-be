4162f8e8543dd05efc9215855eff30cae6414d65
ehgur062300 <ehgur062300@gmail.com>
2024-11-12 21:46:27 +0900

    refactor: 스웨거 코드 추가

--- src/main/java/org/example/autoreview/domain/review/controller/ReviewController.java
+++ src/main/java/org/example/autoreview/domain/review/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package org.example.autoreview.domain.review.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.autoreview.domain.review.dto.request.ReviewDeleteRequestDto;
@@ -21,24 +22,29 @@ import java.util.List;
 @RestController
 public class ReviewController {
 
+
     private final ReviewCodePostService reviewCodePostService;
 
+    @Operation(summary = "리뷰 저장")
     @PostMapping("/save")
     public ApiResponse<String> save(ReviewSaveRequestDto requestDto) {
         reviewCodePostService.save(requestDto);
         return ApiResponse.success(HttpStatus.OK, "Review saved");
     }
 
+    @Operation(summary = "사용자 단일 조회")
     @GetMapping("/find-one")
     public ApiResponse<ReviewResponseDto> findOne(Long id) {
         return ApiResponse.success(HttpStatus.OK, reviewCodePostService.findOne(id));
     }
 
+    @Operation(summary = "리뷰 전체 조회")
     @GetMapping("/find-all")
     public ApiResponse<List<ReviewResponseDto>> findAll() {
         return ApiResponse.success(HttpStatus.OK, reviewCodePostService.findAll());
     }
 
+    @Operation(summary = "리뷰 수정", description = "사용자 검사 후 수정")
     @PutMapping("/update")
     public ApiResponse<String> update(ReviewUpdateRequestDto requestDto,
                                       @AuthenticationPrincipal UserDetails userDetails) {
@@ -46,6 +52,7 @@ public class ReviewController {
         return ApiResponse.success(HttpStatus.OK, "Review updated");
     }
 
+    @Operation(summary = "리뷰 삭제", description = "사용자 검사 후 삭제")
     @DeleteMapping("/delete")
     public ApiResponse<String> delete(ReviewDeleteRequestDto requestDto,
                                       @AuthenticationPrincipal UserDetails userDetails) {
--- src/main/java/org/example/autoreview/domain/review/dto/request/ReviewUpdateRequestDto.java
+++ src/main/java/org/example/autoreview/domain/review/dto/request/ReviewUpdateRequestDto.java
@@ -1,14 +1,22 @@
 package org.example.autoreview.domain.review.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ReviewUpdateRequestDto {
 
+    @Schema(description = "리뷰 아이디", example = "1")
     private final Long id;
+
+    @Schema(description = "사용자 검증을 위한 이메일 정보", example = "abc@naver.com")
     private final String email;
+
+    @Schema(description = "복습 코드에 대한 해설", example = "링가르디움 레비오사")
     private final String description;
+
+    @Schema(description = "복습 코드", example = "HTML도 컴퓨터 언어입니다..")
     private final String code;
 
     @Builder
