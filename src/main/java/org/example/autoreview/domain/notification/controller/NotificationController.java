7990f3e9958d73a2e6a756ab79d32095e38cd758
ehgur062300 <ehgur062300@gmail.com>
2024-11-14 16:44:12 +0900

    refactor(save): 회원 정보 헤더에서 받아오게 수정

--- src/main/java/org/example/autoreview/domain/notification/controller/NotificationController.java
+++ src/main/java/org/example/autoreview/domain/notification/controller/NotificationController.java
@@ -2,7 +2,6 @@ package org.example.autoreview.domain.notification.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.autoreview.domain.notification.dto.request.NotificationSaveRequestDto;
 import org.example.autoreview.domain.notification.dto.response.NotificationResponseDto;
@@ -10,14 +9,11 @@ import org.example.autoreview.domain.notification.service.NotificationService;
 import org.example.autoreview.domain.scheduler.NotificationScheduler;
 import org.example.autoreview.global.exception.response.ApiResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "알림 API", description = "알림 API")
 @RequestMapping("/v1/api/notification")
@@ -30,10 +26,10 @@ public class NotificationController {
 
     @Operation(summary = "알림 저장", description = "회원 정보는 헤더에서")
     @PostMapping("/save")
-    public ApiResponse<String> save(@RequestParam String email, //TODO: 헤더에서 가져오는걸로 수정해야함
+    public ApiResponse<String> save(@AuthenticationPrincipal UserDetails userDetails,
                                     @RequestBody NotificationSaveRequestDto requestDto) {
 
-        notificationService.save(email, requestDto);
+        notificationService.save(userDetails.getUsername(), requestDto);
         return ApiResponse.success(HttpStatus.OK,"save success");
     }
 
