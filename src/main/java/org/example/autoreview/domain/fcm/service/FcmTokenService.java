fc5dd19e8beafcb3adb2afb74ae4999bc1006ca4
ehgur <ehgur062300@gmail.com>
2024-11-17 12:34:37 +0900

    feat: firebase에 데이터 전송하는 메서드 구현

--- src/main/java/org/example/autoreview/domain/fcm/service/FcmTokenService.java
+++ src/main/java/org/example/autoreview/domain/fcm/service/FcmTokenService.java
@@ -1,5 +1,11 @@
 package org.example.autoreview.domain.fcm.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.autoreview.domain.fcm.dto.request.FcmTokenSaveRequestDto;
@@ -22,5 +28,32 @@ public class FcmTokenService {
         FcmToken fcmToken = requestDto.toEntity(member);
         return fcmTokenRepository.save(fcmToken).getId();
     }
+
+    public void pushNotification(List<FcmToken> fcmTokens, String title, String content) {
+        for (FcmToken fcmToken : fcmTokens) {
+            CompletableFuture.runAsync(() -> {
+                try {
+                    String message = FirebaseMessaging.getInstance().send(Message.builder()
+                            .setNotification(Notification.builder()
+                                    .setTitle(title)
+                                    .setBody(content)
+                                    .build())
+                            .setToken(fcmToken.getToken())
+                            .build());
+
+                    fcmToken.updateDate();
+
+                    log.info("Member: {} / Device: {} / Message: {}", fcmToken.getMember().getEmail(), fcmToken.getId(), message);
+                    log.info("Last Used Date: {}", fcmToken.getLastUsedDate());
+
+                } catch (FirebaseMessagingException e) {
+                    log.error("Failed to send message to device {}: {}", fcmToken.getId(), e.getMessage());
+                } catch (Exception e) {
+                    log.error("An unexpected error occurred: {}", e.getMessage());
+                }
+            });
+        }
+    }
+
 
 }
