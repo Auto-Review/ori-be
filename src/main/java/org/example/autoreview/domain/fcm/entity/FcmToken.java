725b8a2004fd54118055a7657ce26faf3f053d1d
ehgur <ehgur062300@gmail.com>
2024-11-17 12:33:39 +0900

    feat: 마지막 사용 시간 수정 메서드 추가

--- src/main/java/org/example/autoreview/domain/fcm/entity/FcmToken.java
+++ src/main/java/org/example/autoreview/domain/fcm/entity/FcmToken.java
@@ -1,12 +1,17 @@
 package org.example.autoreview.domain.fcm.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.autoreview.domain.member.entity.Member;
-
-import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
@@ -28,6 +33,10 @@ public class FcmToken {
     public FcmToken(Member member, String token) {
         this.member = member;
         this.token = token;
+        this.lastUsedDate = LocalDate.now();
+    }
+
+    public void updateDate() {
         this.lastUsedDate = LocalDate.now();
     }
 }
