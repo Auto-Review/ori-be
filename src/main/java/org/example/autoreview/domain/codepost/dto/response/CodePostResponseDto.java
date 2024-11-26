779a83db055994ff3ad84a7163915c8ade15d3a8
ehgur062300 <ehgur062300@gmail.com>
2024-11-26 14:56:20 +0900

    feat: 코드 언어 필드 추가

--- src/main/java/org/example/autoreview/domain/codepost/dto/response/CodePostResponseDto.java
+++ src/main/java/org/example/autoreview/domain/codepost/dto/response/CodePostResponseDto.java
@@ -1,12 +1,13 @@
 package org.example.autoreview.domain.codepost.dto.response;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.Getter;
 import org.example.autoreview.domain.codepost.entity.CodePost;
 import org.example.autoreview.domain.member.dto.MemberResponseDto;
 import org.example.autoreview.domain.review.dto.response.ReviewResponseDto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class CodePostResponseDto {
@@ -17,6 +18,7 @@ public class CodePostResponseDto {
     private final int level;
     private final LocalDate reviewDay;
     private final String description;
+    private final String language;
     private final String code;
     private final List<ReviewResponseDto> dtoList;
     private final LocalDateTime createDate;
@@ -28,6 +30,7 @@ public class CodePostResponseDto {
         this.level = entity.getLevel();
         this.reviewDay = entity.getReviewDay();
         this.description = entity.getDescription();
+        this.language = entity.getLanguage().getType();
         this.code = entity.getCode();
         this.dtoList = dtoList;
         this.createDate = entity.getCreateDate();
--- src/main/java/org/example/autoreview/domain/codepost/entity/CodePost.java
+++ src/main/java/org/example/autoreview/domain/codepost/entity/CodePost.java
@@ -1,16 +1,7 @@
 package org.example.autoreview.domain.codepost.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,14 +45,18 @@ public class CodePost extends BaseEntity {
     @Column(length = 1000)
     private String code;
 
+    @Enumerated(EnumType.STRING)
+    private Language language;
+
     @Builder
-    public CodePost(String title, int level, LocalDate reviewDay, String description, String code, Member member) {
+    public CodePost(String title, int level, LocalDate reviewDay, String description, String code, Language language, Member member) {
         this.title = title;
         this.member = member;
         this.level = level;
         this.reviewDay = reviewDay;
         this.description = description;
         this.code = code;
+        this.language = language;
     }
 
     public void update(CodePostUpdateRequestDto requestDto){
@@ -69,6 +64,7 @@ public class CodePost extends BaseEntity {
         this.level = requestDto.getLevel();
         this.reviewDay = requestDto.getReviewDay();
         this.description = requestDto.getDescription();
+        this.language = Language.of(requestDto.getLanguage());
         this.code = requestDto.getCode();
     }
 }
