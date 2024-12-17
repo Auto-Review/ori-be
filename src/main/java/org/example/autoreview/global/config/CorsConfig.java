9af22edb3868a05638f7e87da29f54a6beab9331
신종신 <jongsin16@naver.com>
2024-12-17 16:08:38 +0900

    feat: dev 서버 ci 분리

--- src/main/java/org/example/autoreview/global/config/CorsConfig.java
+++ src/main/java/org/example/autoreview/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.example.autoreview.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.url}")
+    private String url;
+
     @Bean
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://autoreview.kr"); // 허용할 출처
+        config.addAllowedOrigin(url); // 허용할 출처
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("accessToken");
