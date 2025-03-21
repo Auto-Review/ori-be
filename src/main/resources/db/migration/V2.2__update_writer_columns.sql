7bcab34a4e66ef56daeeb70fb8396e67eb3683a0
ehgur062300 <ehgur062300@gmail.com>
2025-03-21 14:19:29 +0900

    refactor: flyway config 설정 파일 작성 및 스크립트 세분화

--- src/main/resources/db/migration/V2.2__update_writer_columns.sql
+++ src/main/resources/db/migration/V2.2__update_writer_columns.sql
@@ -1,15 +1,3 @@
--- CodePost 테이블에 writer_id, writer_email, writer_nickname 컬럼 추가
-ALTER TABLE code_post
-    ADD COLUMN writer_id BIGINT,
-    ADD COLUMN writer_email VARCHAR(255),
-    ADD COLUMN writer_nickname VARCHAR(255);
-
--- TILPost 테이블에 writer_id, writer_email, writer_nickname 컬럼 추가
-ALTER TABLE tilpost
-    ADD COLUMN writer_id BIGINT,
-    ADD COLUMN writer_email VARCHAR(255),
-    ADD COLUMN writer_nickname VARCHAR(255);
-
 -- 기존 member_id 값을 writer_id로 복사
 UPDATE code_post cp
     SET cp.writer_id = cp.member_id
