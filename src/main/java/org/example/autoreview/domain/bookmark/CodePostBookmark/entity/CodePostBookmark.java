a573b35c113773c4344122859dd88f333c11aac3
ehgur062300 <ehgur062300@gmail.com>
2025-06-02 01:13:19 +0900

    refactor: 북마크 save or update 동시성 문제 해결

--- src/main/java/org/example/autoreview/domain/bookmark/CodePostBookmark/entity/CodePostBookmark.java
+++ src/main/java/org/example/autoreview/domain/bookmark/CodePostBookmark/entity/CodePostBookmark.java
@@ -1,13 +1,6 @@
 package org.example.autoreview.domain.bookmark.CodePostBookmark.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +10,9 @@ import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @NoArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(name = "uq_codepost_member",
+        columnNames = {"code_post_id", "member_id"})}
+)
 @Entity
 public class CodePostBookmark extends BaseEntity {
 
