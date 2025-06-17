6c84353dab5752e26880af1c4eb808a1cb9ddd3b
ehgur <ehgur062300@gmail.com>
2025-06-17 10:31:37 +0900

    refactor: 테스트 환경 설정 방식 변경

--- src/test/java/org/example/autoreview/domain/bookmark/CodePostBookmark/service/CodePostBookmarkServiceIntegrationTest.java
+++ src/test/java/org/example/autoreview/domain/bookmark/CodePostBookmark/service/CodePostBookmarkServiceIntegrationTest.java
@@ -17,7 +17,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("${spring.profiles.active:test}")
 @SpringBootTest
 class CodePostBookmarkServiceIntegrationTest {
 
